229a2e091d06c49ce195b3d65e260197898b3442
Roksana <roksana.traczyk@wp.pl>
2024-01-06 19:30:22 +0100

    Implemented task 14.12.2023

--- tasks/2023-12-14/index.ts
+++ tasks/2023-12-14/index.ts
@@ -6,5 +6,20 @@ export type Gift = {
 };
 
 export function calculateMaxGiftValue(gifts: Gift[], maxWeight: number, maxVolume: number): number {
-    return 0;
+    const sortedGifts = gifts.sort((a, b) => b.value - a.value)
+        .filter(gift => gift.weight <= maxWeight && gift.volume <= maxVolume);
+
+    let value = 0;
+    let currentWeight = 0;
+    let currentVolume = 0;
+
+    sortedGifts.forEach(gift => {
+        if (gift.volume + currentVolume <= maxVolume && gift.weight + currentWeight <= maxWeight) {
+            currentWeight += gift.weight;
+            currentVolume += gift.volume;
+            value += gift.value;
+        }
+    })
+
+    return value
 }
